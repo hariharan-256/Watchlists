5c6293667ebf1260d79f1c5d6f17f442f8a2c7f9
Hari <hariharang25@gmail.com>
2024-07-20 02:29:05 +0530

    consoles removed

--- src/pages/MovieSearch.tsx
+++ src/pages/MovieSearch.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import MovieList from './MovieList';
-import { LayOut } from '../organisms/Layout';
+import React from "react";
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
+import { LayOut } from "../organisms/Layout";
 
 const MovieSearch: React.FC = () => {
-    const { searchResults, fetching } = useSelector((state: any) => state.movies);
-    console.log(searchResults, "llk");
+	const { searchResults, fetching } = useSelector((state: any) => state.movies);
 
-
-    return (
-        <LayOut>
-            <div className='text-white'>
-                <MovieList movies={searchResults} fetching={fetching} />
-            </div>
-        </LayOut>
-    );
+	return (
+		<LayOut>
+			<div className="text-white">
+				<MovieList movies={searchResults} fetching={fetching} />
+			</div>
+		</LayOut>
+	);
 };
 
 export default MovieSearch;
